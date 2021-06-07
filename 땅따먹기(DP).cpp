052a5c868eb40438778d544b65698dd0eee3d639
lyd0223 <lyd0223@gmail.com>
2021-06-07 16:11:43 +0900

    Create 땅따먹기(DP).cpp

---
+++ 땅따먹기(DP).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//동적계획법
+//전까지의 최대의 점수를 저장해둔 뒤, 계산.
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+
+    for (int i = 0; i < land.size() - 1; i++)
+    {
+        land[i + 1][0] += max(land[i][1], max(land[i][2], land[i][3]));
+        land[i + 1][1] += max(land[i][0], max(land[i][2], land[i][3]));
+        land[i + 1][2] += max(land[i][0], max(land[i][1], land[i][3]));
+        land[i + 1][3] += max(land[i][0], max(land[i][1], land[i][2]));
+    }
+    sort(land[land.size() - 1].begin(), land[land.size() - 1].end());
+    return land[land.size() - 1][3];
+}
