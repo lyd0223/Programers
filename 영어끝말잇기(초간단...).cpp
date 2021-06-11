c9c20a2ea41d2d0daaead6fcb1e49e7c55aed54e
lyd0223 <lyd0223@gmail.com>
2021-06-12 03:03:22 +0900

    Create 영어끝말잇기(초간단...).cpp

---
+++ 영어끝말잇기(초간단...).cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+//초간단문제...
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
+    unordered_map<string, bool> um;
+
+    um[words[0]] = 1;
+    for (int i = 1; i < words.size(); i++)
+    {
+        if (um[words[i]] == 1)
+        {
+            answer.push_back(i % n + 1);
+            answer.push_back(i / n + 1);
+            return answer;
+        }
+        else
+        {
+            if (words[i - 1][words[i - 1].size() - 1] != words[i][0])
+            {
+                answer.push_back(i % n + 1);
+                answer.push_back(i / n + 1);
+                return answer;
+            }
+            else
+                um[words[i]] = 1;
+        }
+
+    }
+    if (answer.empty())
+    {
+        answer.push_back(0);
+        answer.push_back(0);
+    }
+    return answer;
+}
