52d6f0325d302f48fad5bdf3493318f768165ee8
lyd0223 <lyd0223@gmail.com>
2021-06-19 10:09:23 +0900

    Create 점프와 순간이동(초간단).cpp

---
+++ 점프와 순간이동(초간단).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//초간단 공식문제.
+
+int solution(int n)
+{
+    int ans = 0;
+
+    while (n != 0)
+    {
+        if (n % 2 == 1)
+        {
+            ans++;
+            n--;
+        }
+        n /= 2;
+    }
+
+    return ans;
+}
