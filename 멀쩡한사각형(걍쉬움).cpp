26366672eb6f12cdcb0c3f173f48189a31edfe9c
lyd0223 <lyd0223@gmail.com>
2021-06-27 18:54:30 +0900

    Create 멀쩡한사각형(걍쉬움).cpp

---
+++ 멀쩡한사각형(걍쉬움).cpp
@@ -0,0 +1,22 @@
+﻿#include <algorithm>
+using namespace std;
+
+//초간단 문제라 뭐 설명할게없다...
+//w,h중 큰수 - 작은수를 계속 빼주면된다.
+long long solution(int w, int h) {
+    long long answer = (long)w * h;
+    int maxnum = max(w, h);
+    int minnum = min(w, h);
+
+    int temp = 1000000000;
+    while (temp != 0)
+    {
+        temp = maxnum - minnum;
+        answer -= minnum;
+        maxnum = max(temp, minnum);
+        minnum = min(temp, minnum);
+    }
+
+
+    return answer;
+}
