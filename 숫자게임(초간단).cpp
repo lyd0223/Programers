63cf5366a5cb1a88cc5f5c4b0e1a933408643bbf
lyd0223 <lyd0223@gmail.com>
2021-06-30 23:59:55 +0900

    Create 숫자게임(초간단).cpp

---
+++ 숫자게임(초간단).cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//간단문제...
+//하나씩 비교해가면서 큰거면 올림
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int j = 0;
+    for (int i = 0; i < B.size(); i++)
+    {
+        if (j > A.size())
+            break;
+        if (A[j] < B[i])
+        {
+            j++;
+            answer++;
+        }
+    }
+    return answer;
+}
