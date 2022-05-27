386e5431172114c01fde001a45f76645a299ddc3
Naebaim <lyd0223@gmail.com>
2022-05-27 15:05:20 +0900

    Create 숫자 문자열과 영단어(문자열).cpp

---
+++ 문자열/숫자 문자열과 영단어(문자열).cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    vector<string> numStr = { "zero", "one" , "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    
+    int answer = 0;
+    string answerStr = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            answerStr += s[i];
+            continue;
+        }
+        for (int j = 0; j < numStr.size(); j++)
+        {
+            if (!numStr[j].compare(0, numStr[j].size(), s, i, numStr[j].size()))
+            {
+                answerStr += to_string(j);
+            }
+        }
+    }
+    answer = stoi(answerStr);
+}
